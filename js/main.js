423b311ba9f5bf24f823f2dac9bd3f5e49352927
tramirez30 <ramirez.tomas@yahoo.com>
2024-11-29 17:35:39 -0800

    Debuggin dropdown logic (open/close)

--- js/main.js
+++ js/main.js
@@ -110,11 +110,13 @@ let isActive = (e) => {
   let dropDownMenu = document.querySelector(".dropdown-menu");
 
   // Logic will go here (Check if dropdown was clicked or not)
-
+  if (dropDownInput.contains(e.target)) {
+    dropDownInput.classList.toggle("is-active");
+  } else if (!dropDownMenu.contains(e.target)) {
+    classList.toggle("dropdown-hidden");
+  }
   console.log(dropDownInput.contains(e.target));
   console.log(dropDownMenu.contains(e.target));
-
-  dropDownInput.classList.toggle("is-active");
 };
 
 // Event Listener, runs Quote() after click
