2aeb2199208ee09a8d706a0a67f256ce4dbb20d6
tramirez30 <ramirez.tomas@yahoo.com>
2024-12-14 21:00:41 -0800

    Updating event handler isActive

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ document.querySelector("#getQuote-btn").addEventListener("click", quote);
 document.querySelector("#clear-btn").addEventListener("click", clearQuote);
 
 // Toggle dropdown state
-document.querySelector("#dropdown").addEventListener("click", isActive);
+Window.querySelector("#dropdown").addEventListener("click", isActive);
 
 // If there is no value in the input field, don't display card
 
