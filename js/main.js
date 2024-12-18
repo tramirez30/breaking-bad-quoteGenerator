67a484e5b3aaf3bb13ce7a47419fc5e0b5e7eb71
tramirez30 <ramirez.tomas@yahoo.com>
2024-12-17 21:06:33 -0800

    Adding logic to update icon when an option is selected

--- js/main.js
+++ js/main.js
@@ -117,6 +117,9 @@ let isActive = (e) => {
     // Update icon to switch from up to down and vice versa
     dropDown.classList.toggle("is-active");
     icon.classList = "fas fa-angle-up";
+    if (option.contains(e.target)) {
+      icon.classList = "fas fa-angle-down";
+    }
   } else {
     closeDropdown();
   }
