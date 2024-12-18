3e73d61d2c7a90451a059ca99f227976827d2dd7
tramirez30 <ramirez.tomas@yahoo.com>
2024-12-17 21:17:13 -0800

    Continuing debugging

--- js/main.js
+++ js/main.js
@@ -120,12 +120,14 @@ let isActive = (e) => {
   } else {
     closeDropdown();
   }
+  optionSelected();
 };
 
 // Option function selected
 
 let optionSelected = (e) => {
-  if (option) {
+  if (option.contains(e.target)) {
+    dropDown.classList.remove("is-active");
     icon.classList = "fas fa-angle-down";
   }
 };
@@ -135,7 +137,6 @@ let optionSelected = (e) => {
 let closeDropdown = () => {
   dropDown.classList.remove("is-active");
   icon.classList = "fas fa-angle-down";
-  optionSelected();
 };
 
 // Event Listener, runs Quote() after click
